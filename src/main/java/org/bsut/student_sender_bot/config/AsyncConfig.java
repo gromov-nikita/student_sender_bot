7d74cfb86226646749794b3741f0d30dc2f81608
gromov-nikita <hromay.n999@gmail.com>
2024-09-25 15:28:17 +0300

    Событийная архитектура

--- src/main/java/org/bsut/student_sender_bot/config/AsyncConfig.java
+++ src/main/java/org/bsut/student_sender_bot/config/AsyncConfig.java
@@ -1,9 +1,27 @@
 package org.bsut.student_sender_bot.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    @Bean(name = "customExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 4);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+
 }
